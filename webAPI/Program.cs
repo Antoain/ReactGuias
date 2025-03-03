0105e3ced05799c6fc04f9cd4920d3f2aaeddc57
Antony <126409554+Antoain@users.noreply.github.com>
2025-03-03 00:04:55 -0600

    Guia 15

--- webAPI/Program.cs
+++ webAPI/Program.cs
@@ -6,6 +6,9 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddCors(policyBuilder => policyBuilder.AddDefaultPolicy(
+    policy => policy.WithOrigins("*").AllowAnyHeader().AllowAnyMethod()
+    ));
 
 var app = builder.Build();
 
@@ -15,7 +18,7 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
-
+app.UseCors();
 app.UseHttpsRedirection();
 
 app.UseAuthorization();
